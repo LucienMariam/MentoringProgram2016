d30453920c20fd70ca7008fe76543b1cf722c8f6
Lucien <lucien.mariam@gmail.com>
2016-07-13 17:58:11 +0300

    Homework1

---
+++ Epam.MentoringProgram2016.Homework1/Epam.MentoringProgram2016.HW1.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Epam.MentoringProgram2016.Homework1;
+
+namespace Epam.MentoringProgram2016.HW1.Tests
+{
+    [TestClass]
+    public class StrangeNumberGeneratorTest
+    {
+        [TestMethod]
+        public void Array_2_3_5_7_MustBeProcessedAsOutput_6()
+        {
+            int[] sampleArray = { 2, 3, 5, 7 };
+            var generator = new StrangeNumberGenerator(sampleArray);
+
+            Assert.AreEqual(6, generator.GenerateStrangeNumber());
+        }
+
+        [TestMethod]
+        public void Array_2_3_6_8_MustBeProcessedAsOutput_7()
+        {
+            int[] sampleArray = { 2, 3, 6, 8 };
+            var generator = new StrangeNumberGenerator(sampleArray);
+
+            Assert.AreEqual(7, generator.GenerateStrangeNumber());
+        }
+
+        [TestMethod]
+        public void Array_2_4_6_12_MustBeProcessedAsOutput_9()
+        {
+            int[] sampleArray = { 2, 4, 6, 12 };
+            var generator = new StrangeNumberGenerator(sampleArray);
+
+            Assert.AreEqual(9, generator.GenerateStrangeNumber());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ThrowExceptionIfArrayIsEmpty()
+        {
+            int[] sampleArray = null;
+            var generator = new StrangeNumberGenerator(sampleArray);
+        }
+    }
+}
---
+++ Epam.MentoringProgram2016.Homework1/Epam.MentoringProgram2016.Homework1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+
+//We need to create a code for generate a N-length list of simple number, this list will be start with 2. After the list has been generated, we need to get sum all elements of this list divided by index of this element+1 and we need to get integer part of this sum, method Math.Floor()
+//An example:
+//List for 4 elements: 2, 3, 5, 7.
+//Result: Math.Floor( 2/1 + 3/2 + 5/3 + 7/4) = Math.Floor(6.916666666666667) = 6
+
+namespace Epam.MentoringProgram2016.Homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var filler = new ArrayFiller();
+            var generator = new StrangeNumberGenerator(filler.GetArray(4));
+            Debug.WriteLine("==== {0} ====", generator.GenerateStrangeNumber());
+        }
+    }
+
+
+    public class ArrayFiller
+    {
+        public int[] GetArray(int arrLength)
+        {
+            int[] array = new int[arrLength];
+            var generator = new Random(DateTime.Now.Second);
+
+            array[0] = 2;
+            int lastSequenceNumber = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = generator.Next(lastSequenceNumber + 1, lastSequenceNumber * 2 + 1);
+                lastSequenceNumber = array[i];
+            }
+
+            return array;
+        }
+    }
+
+    public class StrangeNumberGenerator
+    {
+        private int[] array;
+        public int[] Array
+        {
+            get
+            {
+                return array;
+            }
+
+            set
+            {
+                if (value.Length > 0)
+                    array = value;
+
+                else throw new MemberAccessException();
+            }
+        }
+
+        public StrangeNumberGenerator(int[] array)
+        {
+            Array = array;
+        }
+
+        public int GenerateStrangeNumber()
+        {
+            double sumAccumulator = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+                sumAccumulator += array[i] / (i + 1.0);
+
+
+            for (int i = 0; i < array.Length; i++)
+                Debug.WriteLine(array[i]);
+
+            Debug.WriteLine("==== {0} ====", sumAccumulator);
+
+            return Convert.ToInt32(Math.Floor(sumAccumulator));
+        }
+    }
+}
