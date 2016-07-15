1b4ea94a35b3a53c499d41b1f07d14edcf87be85
Lucien <lucien.mariam@gmail.com>
2016-07-15 17:20:14 +0300

    Do Homework_Types2

--- Epam.MentoringProgram2016.HW1_Types/Epam.MentoringProgram2016.HW1_Types/Types_2.cs
+++ Epam.MentoringProgram2016.HW1_Types/Epam.MentoringProgram2016.HW1_Types/Types_2.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Epam.MentoringProgram2016.HW1_Types
 {
-    class Types_2
+    // Вам необходимо  описать структуру, которая представляет собой товар в интернет-магазине.Товар имеет наименование, цену и количество едениц на складе.
+    // В дальнейшем вы будете работать со списком товара и осуществлять различные выборки с помощью LINQ to Object.
+    // Спроектируйте и опишите структуру правильно с точки зрения реализации ValueType.
+
+    public struct Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
