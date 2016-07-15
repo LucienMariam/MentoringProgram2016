5df399e9fae9ad1f68ae8a3d3df0c1d06d0f3b13
Lucien <lucien.mariam@gmail.com>
2016-07-15 14:34:09 +0300

    Add Homework1: Types
    Initial state and conditions are set.

---
+++ Epam.MentoringProgram2016.HW1_Types/Epam.MentoringProgram2016.HW1_Types/Types_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.MentoringProgram2016.HW1_Types
+{
+    struct InfoData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Source
+    {
+        internal void CheckAndProceed(List<InfoData> data)
+        {
+            var dest = new Destination();
+
+            //do something
+
+            dest.ProceedData(data);
+        }
+    }
+
+    class Destination
+    {
+        internal void ProceedData(List<InfoData> data)
+        {
+            foreach (var item in data)
+            {
+                //do something
+            }
+        }
+    }
+}
