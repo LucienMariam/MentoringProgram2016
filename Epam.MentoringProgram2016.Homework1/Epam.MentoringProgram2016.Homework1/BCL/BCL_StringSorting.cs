398645c45554c2b973fb166c60881a74da3d3e32
LucienMariam <Alcotrass@protonmail.ch>
2016-08-06 22:28:47 +0300

    Add BCL 2 Hometask

---
+++ Epam.MentoringProgram2016.Homework1/Epam.MentoringProgram2016.Homework1/BCL/BCL_StringSorting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.MentoringProgram2016.Homework1.BCL
+{
+    public class BCL_StringSorting
+    {
+        private readonly List<string> _array;
+
+        public BCL_StringSorting(string[] array)
+        {
+            if(array == null)
+                throw new NullReferenceException();
+
+            _array = new List<string>(array);
+        }
+
+        public string[] AlphabeticSorting()
+        {
+            string temp;
+
+            for (int i = 0; i < _array.Count; i++)
+            {
+                for (int j = i + 1; j < _array.Count; j++)
+                {
+                    if (string.Compare(_array[i], _array[j], StringComparison.Ordinal) > 0)
+                    {
+                        temp = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = temp;
+                    }
+                }
+            }
+
+            return _array.ToArray();
+        }
+    }
+}
