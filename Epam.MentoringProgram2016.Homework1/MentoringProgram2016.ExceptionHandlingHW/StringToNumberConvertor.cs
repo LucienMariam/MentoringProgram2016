7bf08f6af71aab5c479107537e97c6b808080f33
LucienMariam <Alcotrass@protonmail.ch>
2016-08-16 22:06:56 +0300

    Add Exception Handling 2 Hometask

---
+++ Epam.MentoringProgram2016.Homework1/MentoringProgram2016.ExceptionHandlingHW/StringToNumberConvertor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Convertor
+{
+    public static class StringToNumberConvertor
+    {
+        public static bool ConvertToInteger(out int result, string inputString)
+        {
+            result = 0;
+
+            if (string.IsNullOrEmpty(inputString))
+            {
+                return false;
+            }
+            inputString = inputString.Trim();
+            bool isNegative = inputString.StartsWith("-");
+            inputString = inputString.TrimStart('-');
+            inputString = inputString.TrimStart('0');
+
+            if (inputString.ToCharArray().Any(f => f < '0' || f > '9'))
+            {
+                return false;
+            }
+
+            try
+            {
+                checked
+                {
+                    for (int i = inputString.Length - 1, degreeCounter = 0; i >= 0; i--, degreeCounter++)
+                    {
+                        result += (inputString[i] - '0')*(int) Math.Pow(10, degreeCounter);
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+
+            if (isNegative)
+                result = -result;
+            return true;
+        }
+    }
+}
