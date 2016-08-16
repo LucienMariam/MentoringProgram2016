e8f2441d7aecdf1ee1beb902cd13c68150b1183f
LucienMariam <Alcotrass@protonmail.ch>
2016-08-16 21:46:03 +0300

    Add Exception Handling 1 Homework

---
+++ MentoringProgram2016.ExceptionHandlingHW/MentoringProgram2016.ExceptionHandlingHW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MentoringProgram2016.ExceptionHandlingHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string WordToStopInput = "1111";
+            string inputString;
+
+            do
+            {
+                inputString = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(inputString.First());
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("The string is empty");
+                }
+            } while (inputString != WordToStopInput);
+        }
+    }
+}
