f16ac6176809a24a47e02ca3cec24d8ac280ab90
LucienMariam <Alcotrass@protonmail.ch>
2016-08-26 01:39:17 +0300

    Add Reflection hometask1

---
+++ Epam.MentoringProgram2016.Homework1/Mentoring2016.Reflection/Hometask1_CreateListT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace Mentoring2016.Reflection
+{
+    class ListTReflectionCreator<T> where T : new()
+    {
+        public List<T> CreateList()
+        {
+            var list = (List<T>)Activator.CreateInstance(typeof(List<T>));
+            Type listElement = list.GetType().GetTypeInfo().GetElementType();
+
+            if (listElement.IsValueType || listElement.IsEquivalentTo(typeof(string)))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    list.Add(new T());
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Epam.MentoringProgram2016.Homework1/Mentoring2016.Reflection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentoring2016.Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
