d358587b444ff9f1f8948fae36f385146a96fa98
Lucien <lucien.mariam@gmail.com>
2016-08-29 20:00:16 +0300

    Add Hometask2 EventHadler via Reflection

---
+++ Epam.MentoringProgram2016.Homework1/Mentoring2016.Reflection/Hometask2_AddHandlerToClassEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mentoring2016.Reflection
+{
+    public class Friday13Manager
+    {
+        public event Func<int> Friday13;
+    }
+
+    public class Friday13Handler
+    {
+        public int ReturnDayOfTheFriday13()
+        {
+            return 13;
+        }
+    }
+}
---
+++ Epam.MentoringProgram2016.Homework1/MentoringProgram2016.Reflection.Tests/Hometask2_AddHandlerToClassEvent_Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mentoring2016.Reflection;
+
+namespace MentoringProgram2016.Reflection.Tests
+{
+    [TestClass]
+    public class Hometask2_AddHandlerToClassEvent_Test
+    {
+        [TestMethod]
+        public void SomeMethod()
+        {
+            var friday13EventManager = new Friday13Manager();
+            var friday13Handler = new Friday13Handler();
+            EventInfo eventInfo = friday13EventManager.GetType().GetEvent("Friday13");
+            //MethodInfo methodInfo = friday13Handler.GetType().GetMethod("ReturnDayOfTheFriday13");
+            Func<int> dateGetter = () => friday13Handler.ReturnDayOfTheFriday13();
+            
+            eventInfo.AddEventHandler(friday13EventManager, dateGetter);
+ 
+
+            var actual = dateGetter.Invoke();
+            var expected = 13;
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
