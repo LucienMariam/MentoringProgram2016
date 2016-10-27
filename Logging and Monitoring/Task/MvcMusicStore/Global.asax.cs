eb26febd9744a4057f9930a29da7ea387e2e6204
LucienMariam <Alcotrass@protonmail.ch>
2016-10-27 15:14:20 +0300

    Add Logging and Monitoring projects

---
+++ Logging and Monitoring/Task/MvcMusicStore/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using NLog;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private readonly ILogger logger;
+
+        public MvcApplication()
+        {
+            this.logger = LogManager.GetCurrentClassLogger();
+        }
+
+        protected void Application_Start()
+        {
+            this.logger.Info("Application has started!");
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            using (var loginCounter = new PerformanceCounter("High Category", "PerformoCounter", false))
+            {
+                loginCounter.RawValue = 0;
+            }
+        }
+
+        protected void Application_Error()
+        {
+            Exception ex = Server.GetLastError();
+            logger.Error(ex.ToString());
+        }
+    }
+}
---
+++ Logging and Monitoring/Task/RegisterPerformanceCounters/RegisterPerformanceCounters/PerformanceCountersRegistration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace RegisterPerformanceCounters
+{
+    public class PerformanceCountersRegistration
+    {
+        private const string CategoryName = "High Category";
+        private const string CounterName = "PerformoCounter";
+        private const string LoginCounterHelp = "Counts the successful Login attempts";
+
+        static void Main(string[] args)
+        {
+            if (!PerformanceCounterCategory.Exists(CategoryName))
+            {
+                var counterDataCollection = new CounterCreationDataCollection();
+                var loginCounter = new CounterCreationData(CounterName, LoginCounterHelp,
+                    PerformanceCounterType.NumberOfItems32);
+
+                counterDataCollection.Add(loginCounter);
+
+                try
+                {
+                    PerformanceCounterCategory.Create(CategoryName, "", PerformanceCounterCategoryType.MultiInstance,
+                        counterDataCollection);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                   Console.WriteLine("Do not have permissions");
+                   Console.ReadKey();
+                }
+                Console.WriteLine("Category created");
+                Console.ReadKey();
+            }
+        }
+    }
+}
